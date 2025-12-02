537613fe2ad8494ae214e8d9439e08d410ce0bd0
Franco-Fabregat <fabregatsierra15@gmail.com>
2025-12-02 17:09:42 -0300

    Update modales.js

--- javascript/modales.js
+++ javascript/modales.js
@@ -9,16 +9,18 @@ function activarModal() {
     contenedorModal.classList.add("opacity");
     contenedorModal.innerHTML = `
       <div class="modal-contenido">
-        Hola!... Soy <strong>Franco Fabregat</strong>, un Frontend Developer en formación, apasionado por
-        crear experiencias digitales atractivas y funcionales. Mi objetivo es
-        combinar creatividad y tecnología para diseñar lugares webs que no solo
-        sean visualmente impresionantes, sino también fáciles de usar y
-        accesibles para todos. Con habilidades en HTML, CSS, JavaScript,
-        también sigo aprendiendo nuevas tecnologías y habilidades como el uso de frameworks tales
-        como React y Bootstrap y librerías como jQuery ya que es un mundo muy vasto.
-        Por ahora domino Bootstrap. Estoy listo para
-        enfrentar nuevos desafíos y contribuir al éxito de proyectos
-        innovadores mientras aprendo y crezco en este mundo tecnológico. Este es mi <strong>portafolio:</strong>
+      Hola!... Soy <strong>Franco Fabregat</strong>, un Frontend Developer en formación,
+      apasionado por crear experiencias digitales atractivas y funcionales.
+      Mi objetivo es combinar creatividad y tecnología para diseñar sitios web que no solo sean visualmente impresionantes,
+      sino también fáciles de usar y accesibles para todos.
+<br>
+Cuento con habilidades en <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong> y
+<strong>Bootstrap</strong>. Actualmente continúo ampliando mis conocimientos en <strong>JavaScript</strong>,
+y luego estaré explorando nuevas tecnologías, como el uso de frameworks entre ellos <strong>React</strong> y
+librerías como <strong>jQuery</strong>, ya que el mundo del desarrollo web es amplio y siempre está en evolución.
+Por ahora, domino especificamente JavaScript orientado al DOM para crear interfaces interactivas.
+<br>
+Bienvenido a mi <strong>portafolio:</strong>
         <button class="salir">❌</button>
       </div>
     `;
@@ -51,10 +53,10 @@ const proyectos = {
       "Desarrollé un sitio web para una pastelería local llamada 'Dulce Encanto'. El sitio presenta un diseño atractivo y funcional, con secciones para mostrar productos, promociones y contacto. Utilicé HTML, CSS y JavaScript para crear una experiencia de usuario amigable y visualmente atractiva.",
     tecnologias: ["HTML", "CSS"],
     Imagen: [
-      "visuales/capturas-de-proyectos/pasteleria/photo1.png",
-      "visuales/capturas-de-proyectos/pasteleria/photo2.jpg",
-      "visuales/capturas-de-proyectos/pasteleria/photo3.jpg",
-      "visuales/capturas-de-proyectos/pasteleria/photo4.jpg",
+      "/practicas.franco/visuales/capturas-de-proyectos/pasteleria/photo1.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/pasteleria/photo2.jpg",
+      "/practicas.franco/visuales/capturas-de-proyectos/pasteleria/photo3.jpg",
+      "/practicas.franco/visuales/capturas-de-proyectos/pasteleria/photo4.jpg",
     ],
   },
   proyecto2: {
@@ -64,8 +66,8 @@ const proyectos = {
       "Creé un sitio web básico y de un restaurante local llamado 'Sabor de Barrio'. El sitio incluye fotos de los platillos y menu, horarios de apertura y contacto. Implementé un diseño responsivo para asegurar que el sitio se vea bien en dispositivos móviles y de escritorio.",
     tecnologias: ["HTML", "CSS"],
     Imagen: [
-      "visuales/capturas-de-proyectos/saborDbarrio/photo1.png",
-      "visuales/capturas-de-proyectos/saborDbarrio/photo2.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/saborDbarrio/photo1.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/saborDbarrio/photo2.png",
     ],
   },
   proyecto3: {
@@ -74,7 +76,7 @@ const proyectos = {
     descripcion:
       "Cloné una página web inspirada en Apple Store, con un diseño moderno y funcional que refleja la identidad de la marca. Muestro un producto nuevo, servicios y contacto.",
     tecnologias: ["HTML", "CSS"],
-    Imagen: ["visuales/capturas-de-proyectos/appleStore/photo1.png"],
+    Imagen: ["/practicas.franco/visuales/capturas-de-proyectos/appleStore/photo1.png"],
   },
   proyecto4: {
     id: "proyecto4",
@@ -83,8 +85,8 @@ const proyectos = {
       "Una galería de productos inspirada en una tienda real, mostrando solo unos pocos productos digitales como ejemplo.",
     tecnologias: ["HTML", "CSS"],
     Imagen: [
-      "visuales/capturas-de-proyectos/galeriaDproductos/photo1.png",
-      "visuales/capturas-de-proyectos/galeriaDproductos/photo2.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/galeriaDproductos/photo1.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/galeriaDproductos/photo2.png",
     ],
   },
   proyecto5: {
@@ -94,10 +96,10 @@ const proyectos = {
       "Tienda de Productos digitales digitales, con diseño responsivo.",
     tecnologias: ["HTML", "CSS", "Bootstrap"],
     Imagen: [
-      "visuales/capturas-de-proyectos/TechStore/photo1.png",
-      "visuales/capturas-de-proyectos/TechStore/photo2.png",
-      "visuales/capturas-de-proyectos/TechStore/photo3.png",
-      "visuales/capturas-de-proyectos/TechStore/photo4.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/TechStore/photo1.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/TechStore/photo2.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/TechStore/photo3.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/TechStore/photo4.png",
     ],
   },
 
@@ -108,10 +110,10 @@ const proyectos = {
       "Famoso juego X y O, un juego que permite a dos jugadores competir en un tablero interactivo. Muestra el estado del juego en tiempo real y determina el ganador o si hay un empate.",
     tecnologias: ["HTML", "CSS", "JavaScript"],
     Imagen: [
-      "visuales/capturas-de-proyectos/3enRaya/photo1.png",
-      "visuales/capturas-de-proyectos/3enRaya/photo2.png",
-      "visuales/capturas-de-proyectos/3enRaya/photo3.png",
-      "visuales/capturas-de-proyectos/3enRaya/photo4.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/3enRaya/photo1.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/3enRaya/photo2.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/3enRaya/photo3.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/3enRaya/photo4.png",
     ],
   },
   proyecto7: {
@@ -121,9 +123,9 @@ const proyectos = {
       "Aplicación web para gestionar tareas diarias. Permite a los usuarios agregar, marcar como completadas y eliminar tareas, con una interfaz sencilla y funcional.",
     tecnologias: ["HTML", "CSS", "JavaScript"],
     Imagen: [
-      "visuales/capturas-de-proyectos/AppTareas/photo1.png",
-      "visuales/capturas-de-proyectos/AppTareas/photo2.png",
-      "visuales/capturas-de-proyectos/AppTareas/photo3.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/AppTareas/photo1.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/AppTareas/photo2.png",
+      "/practicas.franco/visuales/capturas-de-proyectos/AppTareas/photo3.png",
     ],
   },
 };
@@ -135,8 +137,8 @@ function activarModalProyecto(e) {
     const vistaProyecto = document.createElement("div");
     vistaProyecto.classList.add("opacity");
     vistaProyecto.innerHTML = `
-      <div class="modal-contenido"
-        <h2>${proyecto.titulo}</h2>
+      <div class="modal-contenido">
+        <h2 class ="tituloProyecto">${proyecto.titulo}</h2>
         <p>${proyecto.descripcion}</p>
         <ul>
           ${proyecto.tecnologias.map((tec) => `<li>${tec}</li>`).join("")}
